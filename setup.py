8132276d45c1eee73cb525784165f6a9448fa84b
justinsasek <justinsasek@gmail.com>
2024-02-29 14:44:13 -0600

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rl_final_project',
+    version='0.0.0',
+    author='Justin Sasek and Harshal Bharatia',
+    description='RL Final Project',
+    license="MIT",
+    packages=find_packages(include=["rl_final_project"]),
+    python_requires="==3.10.13",
+    install_requires=[
+        "gymnasium>=0.26",
+        "tqdm",
+        "numpy",
+        "torch",
+        "matplotlib",
+        "wandb",
+    ],
+)
